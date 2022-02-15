560b11b01db1c9a601ba3657cf23102784d61fbb
Sirotin-D <dmitrii.sirotin@mail.ru>
2022-02-16 01:43:40 +0300

    BugFix

--- GraduateWork/MyForm.h
+++ GraduateWork/MyForm.h
@@ -903,7 +903,7 @@ private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e
 
 
 
-}
+};
 private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
 
 	//X and Y Dimension 
